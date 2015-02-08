3407887aff7740a691aaadbd383267bb8009eeca
Johannes Teichrieb <isrvoid@gmail.com>
2015-02-08 02:20:22 +0000

    Add constructors to message types

--- types.d
+++ types.d
@@ -4,18 +4,48 @@ struct MessageId
 {
     ubyte[20] hash;
     MessageSize size;
+
+    this(MessageIdSource source) pure nothrow @safe
+    {
+        import std.digest.sha;
+        SHA1 sha;
+        sha.put(cast(immutable(ubyte)[]) source.name);
+        sha.put(cast(ubyte) source.size.isVariable);
+        sha.put(cast(ubyte[]) [source.size.total]);
+        hash = sha.finish();
+
+        size = source.size;
+    }
+
+    this(in ubyte[] hash, MessageSize size) pure nothrow @safe
+    {
+        this.hash = hash;
+        this.size = size;
+    }
 }
 
 struct NamedMessageId
 {
     string name;
     MessageId id;
+
+    this(MessageIdSource source) pure nothrow @safe
+    {
+        name = source.name;
+        id = MessageId(source);
+    }
 }
 
 struct MessageIdSource
 {
-    struct name;
+    string name;
     MessageSize size;
+
+    this(string name, MessageSize size) pure nothrow @safe
+    {
+        this.name = name;
+        this.size = size;
+    }
 }
 
 struct MessageSize
@@ -23,5 +53,11 @@ struct MessageSize
     bool isVariable;
     uint total;
     alias max = total;
+
+    this(bool isVariableSize, uint size) pure nothrow @safe
+    {
+        isVariable = isVariableSize;
+        total = size;
+    }
 }
 
