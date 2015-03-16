5119940cb010525e5256b06097e87105232b5a31
Johannes Teichrieb <isrvoid@gmail.com>
2015-03-16 19:37:06 +0000

    Remove nameLength from C interface
    Added comment explains the reasoning behind this decision - passing the
    length around without implicitly knowing the length of an array is
    somewhat painful.

--- src/nadam.h
+++ src/nadam.h
@@ -22,16 +22,31 @@ typedef int (*nadam_recv_t)(void *dest, uint32_t n);
 // size argument is intended for messages of variable size
 typedef void (*nadam_delegate_t)(void *msg, uint32_t size, const nadam_messageInfo_t *messageInfo);
 
+/*
+ * Modern languages shouldn't care about a string being zero terminated.
+ * To make C implementation fully compliant, one would need to pass the length with every name.
+ * That would be painful, therefore the follwing compromise was made:
+ * name's length is assumed to be the index of first '\0'.
+ * The limitation of this is that all names truncated at first '\0' have to be unique.
+ * Otherwise nadam_init() would fail. nameLength in nadam_messageInfo_t is endowed
+ * by message type generator utility, but is ignored in this implementation.
+ */
+
 // infoList memory shouldn't be freed until after nadam_initiate() call
 int nadam_init(const nadam_messageInfo_t *messageInfos, size_t infoCount, size_t hashLengthMin);
 
 // if the delegate for a message type is not set, messages of this type are ignored (dumped)
-int nadam_setDelegate(const char *name, size_t nameLength, nadam_delegate_t delegate);
+int nadam_setDelegate(const char *name, nadam_delegate_t delegate);
 
 int nadam_initiate(nadam_send_t send, nadam_recv_t recv);
 
 // size argument is ignored for constant size messages
-int nadam_send(const char *name, size_t nameLength, const void *msg, uint32_t size);
+int nadam_send(const char *name, const void *msg, uint32_t size);
+/*
+ * Calling this send version (Send With Immutable Name) promises, that the name is a string literal or memory,
+ * whose content won't change throughout the life of the program - allowes name lookup caching.
+ */
+int nadam_sendWin(const char *name, const void *msg, uint32_t size);
 
 void nadam_terminate(void);
 
