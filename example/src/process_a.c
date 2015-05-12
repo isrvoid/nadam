b5f1add5c620d48d60c15f721e6dbb923c4e27f4
Johannes Teichrieb <isrvoid@gmail.com>
2015-05-12 22:30:51 +0000

    Fix example C

--- example/src/process_a.c
+++ example/src/process_a.c
@@ -29,20 +29,21 @@ static void recvDelegate(void *msg, uint32_t n, const nadam_messageInfo_t *mi) {
 
 static void errorDelegate(int error) {
     printf("a: error delegate: %d\n", error);
+    conn_close();
 }
 
 int main(void) {
     handleConnection();
 
     nadam_init(nadamMessageInfos, NADAM_MESSAGE_INFO_COUNT, NADAM_MIN_HASH_LENGTH);
-    nadam_setDelegate("PONG", recvDelegate);
-    if (!nadam_initiate(conn_send, conn_recv, errorDelegate))
-        printf("a: nadam handshake successful\n");
+    nadam_setDelegate("pong", recvDelegate);
+    nadam_initiate(conn_send, conn_recv, errorDelegate);
 
+    printf("a: sending 'ping'\n");
     const char *pingMsg = "the quick brown fox";
-    printf("a: sending 'PING'\n");
-    nadam_send("PING", pingMsg, (uint32_t) strlen(pingMsg));
-    sleep(2);
+    nadam_send("ping", pingMsg, (uint32_t) strlen(pingMsg));
 
+    sleep(1);
+    nadam_stop();
     exit(EXIT_SUCCESS);
 }
--- example/src/process_b.c
+++ example/src/process_b.c
@@ -14,6 +14,7 @@ static void recvDelegate(void *msg, uint32_t n, const nadam_messageInfo_t *mi);
 static void errorDelegate(int error);
 
 static void handleConnection(void) {
+    sleep(1); // wait for process_a to create fifo
     conn_open(false);
     atexit(conn_close);
 }
@@ -26,20 +27,21 @@ static void recvDelegate(void *msg, uint32_t n, const nadam_messageInfo_t *mi) {
 
 static void errorDelegate(int error) {
     printf("b: error delegate: %d\n", error);
+    conn_close();
 }
 
 int main(void) {
     handleConnection();
 
     nadam_init(nadamMessageInfos, NADAM_MESSAGE_INFO_COUNT, NADAM_MIN_HASH_LENGTH);
-    nadam_setDelegate("PING", recvDelegate);
-    if (!nadam_initiate(conn_send, conn_recv, errorDelegate))
-        printf("b: nadam handshake successful\n");
+    nadam_setDelegate("ping", recvDelegate);
+    nadam_initiate(conn_send, conn_recv, errorDelegate);
 
+    printf("b: sending 'pong'\n");
     const char *pingMsg = "jumps over the lazy dog";
-    printf("b: sending 'PONG'\n");
-    nadam_send("PONG", pingMsg, (uint32_t) strlen(pingMsg));
-    sleep(1);
+    nadam_send("pong", pingMsg, (uint32_t) strlen(pingMsg));
 
+    sleep(1);
+    nadam_stop();
     exit(EXIT_SUCCESS);
 }
