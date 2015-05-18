db3ad57d31688f7f92302fc0fc360729f3704cb6
Johannes Teichrieb <teichrieb@hameg.com>
2015-05-18 13:22:03 +0200

    Refactor example C
    Extract nadamInit(), add recv delegates, send additional messages.

--- example/src/processA.c
+++ example/src/processA.c
@@ -3,15 +3,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <assert.h>
 
 #include "nadam.h"
 #include "connection.h"
 
 #include "messageInfos.c"
-
-static void handleConnection(void);
-static void recvDelegate(void *msg, uint32_t n, const nadam_messageInfo_t *mi);
-static void errorDelegate(int error);
 
 static void handleConnection(void) {
     conn_mkfifo();
@@ -21,10 +18,23 @@ static void handleConnection(void) {
     atexit(conn_close);
 }
 
-static void recvDelegate(void *msg, uint32_t n, const nadam_messageInfo_t *mi) {
+static void genericStringDelegate(void *msg, uint32_t n, const nadam_messageInfo_t *mi) {
     char *cmsg = msg;
     cmsg[n] = 0;
     printf("a: received '%s': '%s'\n", mi->name, cmsg);
+}
+
+static struct {
+    double duration;
+    uint32_t count;
+} storage;
+
+static void countDelegate(void *msg, uint32_t n, const nadam_messageInfo_t *mi) {
+    printf("a: received '%s': %u\n", mi->name, storage.count);
+}
+
+static void durationDelegate(void *msg, uint32_t n, const nadam_messageInfo_t *mi) {
+    printf("a: received '%s': %f\n", mi->name, storage.duration);
 }
 
 static void errorDelegate(int error) {
@@ -32,16 +42,39 @@ static void errorDelegate(int error) {
     conn_close();
 }
 
+static void verboseSend(const char *name, const void *msg, uint32_t size) {
+    printf("a: sending '%s'\n", name);
+    int error = nadam_send(name, msg, size);
+    assert(!error);
+}
+
+static void initNadam(void) {
+    int errorCollector = 0;
+    errorCollector |= nadam_init(messageInfos, MESSAGE_INFO_COUNT, HASH_LENGTH_MIN);
+
+    errorCollector |= nadam_setDelegate("ping", genericStringDelegate);
+    errorCollector |= nadam_setDelegate("pong", genericStringDelegate);
+    errorCollector |= nadam_setDelegateWithRecvBuffer("Foo count", countDelegate, &storage.count, NULL);
+    errorCollector |= nadam_setDelegateWithRecvBuffer("Bar.duration", durationDelegate, &storage.duration, NULL);
+
+    errorCollector |= nadam_initiate(conn_send, conn_recv, errorDelegate);
+
+    assert(!errorCollector);
+}
+
 int main(void) {
     handleConnection();
 
-    nadam_init(messageInfos, MESSAGE_INFO_COUNT, HASH_LENGTH_MIN);
-    nadam_setDelegate("pong", recvDelegate);
-    nadam_initiate(conn_send, conn_recv, errorDelegate);
+    initNadam();
 
-    printf("a: sending 'ping'\n");
     const char *pingMsg = "the quick brown fox";
-    nadam_send("ping", pingMsg, (uint32_t) strlen(pingMsg));
+    verboseSend("ping", pingMsg, (uint32_t) strlen(pingMsg));
+
+    uint32_t count = 42;
+    verboseSend("Foo count", &count, 0);
+
+    double duration = 123.456;
+    verboseSend("Bar.duration", &duration, 0);
 
     sleep(1);
     nadam_stop();
--- example/src/processB.c
+++ example/src/processB.c
@@ -3,15 +3,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <assert.h>
 
 #include "nadam.h"
 #include "connection.h"
 
 #include "messageInfos.c"
-
-static void handleConnection(void);
-static void recvDelegate(void *msg, uint32_t n, const nadam_messageInfo_t *mi);
-static void errorDelegate(int error);
 
 static void handleConnection(void) {
     sleep(1); // wait for process_a to create fifo
@@ -19,10 +16,23 @@ static void handleConnection(void) {
     atexit(conn_close);
 }
 
-static void recvDelegate(void *msg, uint32_t n, const nadam_messageInfo_t *mi) {
+static void genericStringDelegate(void *msg, uint32_t n, const nadam_messageInfo_t *mi) {
     char *cmsg = msg;
     cmsg[n] = 0;
     printf("b: received '%s': '%s'\n", mi->name, cmsg);
+}
+
+static struct {
+    double duration;
+    uint32_t count;
+} storage;
+
+static void countDelegate(void *msg, uint32_t n, const nadam_messageInfo_t *mi) {
+    printf("b: received '%s': %u\n", mi->name, storage.count);
+}
+
+static void durationDelegate(void *msg, uint32_t n, const nadam_messageInfo_t *mi) {
+    printf("b: received '%s': %f\n", mi->name, storage.duration);
 }
 
 static void errorDelegate(int error) {
@@ -30,16 +40,33 @@ static void errorDelegate(int error) {
     conn_close();
 }
 
+static void verboseSend(const char *name, const void *msg, uint32_t size) {
+    printf("b: sending '%s'\n", name);
+    int error = nadam_send(name, msg, size);
+    assert(!error);
+}
+
+static void initNadam(void) {
+    int errorCollector = 0;
+    errorCollector |= nadam_init(messageInfos, MESSAGE_INFO_COUNT, HASH_LENGTH_MIN);
+
+    errorCollector |= nadam_setDelegate("ping", genericStringDelegate);
+    errorCollector |= nadam_setDelegate("pong", genericStringDelegate);
+    errorCollector |= nadam_setDelegateWithRecvBuffer("Foo count", countDelegate, &storage.count, NULL);
+    errorCollector |= nadam_setDelegateWithRecvBuffer("Bar.duration", durationDelegate, &storage.duration, NULL);
+
+    errorCollector |= nadam_initiate(conn_send, conn_recv, errorDelegate);
+
+    assert(!errorCollector);
+}
+
 int main(void) {
     handleConnection();
 
-    nadam_init(messageInfos, MESSAGE_INFO_COUNT, HASH_LENGTH_MIN);
-    nadam_setDelegate("ping", recvDelegate);
-    nadam_initiate(conn_send, conn_recv, errorDelegate);
+    initNadam();
 
-    printf("b: sending 'pong'\n");
     const char *pingMsg = "jumps over the lazy dog";
-    nadam_send("pong", pingMsg, (uint32_t) strlen(pingMsg));
+    verboseSend("pong", pingMsg, (uint32_t) strlen(pingMsg));
 
     sleep(1);
     nadam_stop();
